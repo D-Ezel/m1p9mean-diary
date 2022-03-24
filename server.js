83eb9e4b8e14b9b35a470aa7e9a5d4afd1590b5c
RakDiary <diarihenikajarakotoarisoa9@gmail.com>
2022-03-24 17:23:34 +0100

    server node configuration

--- server.js
+++ server.js
@@ -9,13 +9,30 @@ var app = express();
 // Define the JSON parser as a default way 
 // to consume and produce data through the 
 // exposed APIs
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
+
+/*  "/api/status"
+ *   GET: Get server status
+ *   PS: it's just an example, not mandatory
+ */
+app.get("/api/status", function (req, res) {
+    res.status(200).json({ status: "UP" });
+});
+
+if(process.env.NODE_ENV === "production") {
+
+    var distDir = __dirname + "/dist/e-kaly";
+    app.use(express.static(distDir));
+
+    app.get("*", function(req, res) {
+        res.sendFile(path.join(distDir, "index.html"))
+    })
+}
 
 // Create link to Angular build directory
 // The `ng build` command will save the result
 // under the `dist` folder.
-var distDir = __dirname + "/dist/";
-app.use(express.static(distDir));
 
 const port = process.env.PORT || 8888;
 
@@ -25,10 +42,3 @@ var server = app.listen(port, function () {
     console.log("App now running on port", port);
 });
 
-/*  "/api/status"
- *   GET: Get server status
- *   PS: it's just an example, not mandatory
- */
-app.get("/api/status", function (req, res) {
-    res.status(200).json({ status: "UP" });
-});
