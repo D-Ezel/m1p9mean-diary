65fa27bf7b12308aaa45f67c2a8c093994919243
RakDiary <diarihenikajarakotoarisoa9@gmail.com>
2022-03-30 00:40:50 +0200

    header responsive

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,17 +6,26 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-
-
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {ReactiveFormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent, NavbarComponent, PageNotFoundComponent],
   imports: [
     CommonModule,
-    MatIconModule
+    MatIconModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   exports: [
-    NavbarComponent
+    NavbarComponent,
+    HeaderComponent
   ]
 })
 export class CoreModule { }
