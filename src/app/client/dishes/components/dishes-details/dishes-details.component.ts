9bf72e2996715efaf8db9042cdff02fc83452dfd
RakDiary <diarihenikajarakotoarisoa9@gmail.com>
2022-04-07 01:03:21 +0200

    design page plats

--- src/app/client/dishes/components/dishes-details/dishes-details.component.ts
+++ src/app/client/dishes/components/dishes-details/dishes-details.component.ts
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dishes-details.component.scss']
 })
 export class DishesDetailsComponent implements OnInit {
-
-  constructor() { }
+  images; 
+  responsiveOptions;
+  constructor() { 
+    this.responsiveOptions = [{
+      breakpoint: '1024px',
+      numVisible: 1,
+      numScroll: 3
+    },{
+      breakpoint: '768px',
+      numVisible: 2,
+      numScroll: 2
+    },
+    {
+        breakpoint: '560px',
+        numVisible: 1,
+        numScroll: 1
+    }];
+  }
 
   ngOnInit(): void {
+    this.images = [
+      {random: 'Random', picture: 'https://picsum.photos/id/944/900/500'},
+      {random: 'Samoa', picture: 'https://picsum.photos/id/1011/900/500'},
+      {random: 'Asparagus Wild', picture: 'https://picsum.photos/id/984/900/500'},
+      {random: 'Cook Island', picture: 'https://picsum.photos/id/944/900/500'},
+      {random: 'Niue', picture: 'https://picsum.photos/id/1011/900/500'},
+      {random: 'American Samoa', picture: 'https://picsum.photos/id/984/900/500'}
+  ];
   }
 
 }
--- src/app/client/dishes/dishes.module.ts
+++ src/app/client/dishes/dishes.module.ts
@@ -1,7 +1,11 @@
+import { MatButtonModule } from '@angular/material/button';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatChipsModule} from '@angular/material/chips';
 import {MatSidenavModule} from "@angular/material/sidenav";
+import {CarouselModule} from 'primeng/carousel';
+import {ButtonModule} from 'primeng/button';
+import {ToastModule} from 'primeng/toast';
 
 import { DishesComponent } from './dishes.component';
 import { DishesCategoryComponent } from './components/dishes-category/dishes-category.component';
@@ -16,7 +20,11 @@ import { RouterModule } from '@angular/router';
     CommonModule,
     MatChipsModule,
     MatSidenavModule,
-    RouterModule
+    RouterModule,
+    CarouselModule,
+    ButtonModule,
+    MatButtonModule,
+    ToastModule
   ]
 })
 export class DishesModule { }
