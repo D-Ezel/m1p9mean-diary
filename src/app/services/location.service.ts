b63d218f421360e10f43d4be5cc593d099c960e8
RakDiary <diarihenikajarakotoarisoa9@gmail.com>
2022-04-11 03:40:17 +0200

    update url api to https

--- src/app/services/location.service.ts
+++ src/app/services/location.service.ts
@@ -7,7 +7,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class LocationService {
   private localUrl = "http://localhost:8888/api/location";
-  private url= "http://"+document.domain+"/api/location"
+  private url= "https://"+document.domain+"/api/location"
  
   constructor(private http:HttpClient) { }
 
